76260bb1e00ad02ff1f0d2d2dbf8ca88db8d7b57
Joel <joel@soot.com>
2026-02-06 21:17:11 +1300

    feat: Enhance print and original painting details display by restructuring data, updating UI, and adding print type diagrams.

--- data/paintings.js
+++ data/paintings.js
@@ -2,145 +2,181 @@ const paintingsData = [
   {
     "id": 5,
     "src": "./5.jpeg",
-    "title": "Whakaraupō / Lyttelton Harbour",
-    "artist": "Ian Cocks",
-    "medium": "Fine art giclée print from an original acrylic painting",
-    "year": "2024",
-    "details": "Original - Sold",
-    "location": "Lyttelton Harbour / Whakaraupō",
-    "imageSize": "500 x 340 mm",
-    "totalSize": "580 x 420 mm",
-    "edition": "Open Edition",
-    "printedBy": "Artful Images",
-    "printingDetails": "Produced to the highest archival standards using pigmented inks on archival quality Epsom Velvet paper.",
-    "prices": {
-      "half": "$225",
-      "full": "$420",
-      "canvasHalf": "$350",
-      "canvasFull": "$580"
+    "title": "Port Levy Wharf",
+    "year": "2025",
+    "location": "View from Puari Road looking towards the Port Levy Wharf",
+    "original": {
+      "sold": true,
+      "medium": "Acrylic on canvas",
+      "size": "500 x 280 mm",
+      "price": "Sold"
     },
-    "size": "40 x 60cm"
+    "giclee": {
+      "edition": "Limited Edition of 25",
+      "printer": "Artful Images",
+      "fullSize": "500 x 280 mm",
+      "border": 40,
+      "halfSize": "250 x 140 mm",
+      "prices": {
+        "half": "$225",
+        "full": "$420"
+      }
+    },
+    "canvas": {
+      "prices": {
+        "half": "$350",
+        "full": "$580"
+      }
+    }
   },
   {
     "id": 3,
     "src": "./3.jpeg",
     "title": "Banks Peninsula Study I",
-    "artist": "Ian Cocks",
-    "medium": "Fine art giclée print from an original acrylic painting",
     "year": "2024",
-    "details": "Original - Sold",
     "location": "Banks Peninsula",
-    "imageSize": "300 x 200 mm",
-    "totalSize": "380 x 280 mm",
-    "edition": "Open Edition",
-    "printedBy": "Artful Images",
-    "printingDetails": "Produced to the highest archival standards using pigmented inks on archival quality Epsom Velvet paper.",
-    "prices": {
-      "half": "$180",
-      "full": "$350",
-      "canvasHalf": "$280",
-      "canvasFull": "$480"
+    "original": {
+      "sold": true,
+      "medium": "Acrylic on canvas",
+      "size": "300 x 200 mm",
+      "price": "Sold"
     },
-    "size": "30 x 40cm"
+    "giclee": {
+      "edition": "Limited Edition of 25",
+      "printer": "Artful Images",
+      "fullSize": "300 x 200 mm",
+      "border": 40,
+      "halfSize": "150 x 100 mm",
+      "prices": {
+        "half": "$180",
+        "full": "$350"
+      }
+    },
+    "canvas": {
+      "prices": {
+        "half": "$280",
+        "full": "$480"
+      }
+    }
   },
   {
     "id": 4,
     "src": "./4.jpeg",
     "title": "Banks Peninsula Study II",
-    "artist": "Ian Cocks",
-    "medium": "Fine art giclée print from an original acrylic painting",
     "year": "2024",
-    "details": "Original - Sold",
     "location": "Banks Peninsula",
-    "imageSize": "300 x 200 mm",
-    "totalSize": "380 x 280 mm",
-    "edition": "Open Edition",
-    "printedBy": "Artful Images",
-    "printingDetails": "Produced to the highest archival standards using pigmented inks on archival quality Epsom Velvet paper.",
-    "prices": {
-      "half": "$180",
-      "full": "$350",
-      "canvasHalf": "$280",
-      "canvasFull": "$480"
+    "original": {
+      "sold": true,
+      "medium": "Acrylic on canvas",
+      "size": "300 x 200 mm",
+      "price": "Sold"
     },
-    "size": "30 x 40cm"
+    "giclee": {
+      "edition": "Limited Edition of 25",
+      "printer": "Artful Images",
+      "fullSize": "300 x 200 mm",
+      "border": 40,
+      "halfSize": "150 x 100 mm",
+      "prices": {
+        "half": "$180",
+        "full": "$350"
+      }
+    },
+    "canvas": {
+      "prices": {
+        "half": "$280",
+        "full": "$480"
+      }
+    }
   },
   {
     "id": 6,
     "src": "./6.jpeg",
-    "title": "Port Levy Rocks",
-    "artist": "Ian Cocks",
-    "medium": "Fine art giclée print from an original acrylic painting",
+    "title": "Port Levy Wharf Rocks",
     "year": "2025",
-    "details": "Original Available",
-    "location": "View from Wharf Road across Port Levy",
-    "imageSize": "500 x 340 mm",
-    "totalSize": "580 x 420 mm",
-    "edition": "3",
-    "printedBy": "Artful Images",
-    "printingDetails": "This print reproduces my original painting faithfully and is inspired by the landscapes of Banks Peninsula. It has been produced to the highest archival standards using pigmented inks to ensure long-term colour stability and quality and printed on archival quality Epsom Velvet paper.",
-    "prices": {
-      "half": "$280",
-      "full": "$500",
-      "canvasHalf": "$420",
-      "canvasFull": "$650"
+    "location": "View from Puari Road looking towards the Port Levy",
+    "original": {
+      "sold": false,
+      "medium": "Acrylic on canvas",
+      "size": "500 x 250 mm",
+      "price": "$5000"
     },
-    "size": "50 x 70cm",
-    "originalAvailable": true,
-    "originalMedium": "Acrylic on canvas",
-    "originalSize": "500 x 340 mm",
-    "originalPrice": "POA"
+    "giclee": {
+      "edition": "Limited Edition of 25",
+      "printer": "Artful Images",
+      "fullSize": "500 x 250 mm",
+      "border": 40,
+      "halfSize": "250 x 125 mm",
+      "prices": {
+        "half": "$280",
+        "full": "$500"
+      }
+    },
+    "canvas": {
+      "prices": {
+        "half": "$420",
+        "full": "$650"
+      }
+    }
   },
   {
     "id": 7,
     "src": "./7.jpeg",
     "title": "Diamond Harbour",
-    "artist": "Ian Cocks",
-    "medium": "Fine art giclée print from an original acrylic painting",
     "year": "2024",
-    "details": "Original Available",
     "location": "Diamond Harbour",
-    "imageSize": "500 x 340 mm",
-    "totalSize": "580 x 420 mm",
-    "edition": "Open Edition",
-    "printedBy": "Artful Images",
-    "printingDetails": "Produced to the highest archival standards using pigmented inks on archival quality Epsom Velvet paper.",
-    "prices": {
-      "half": "$225",
-      "full": "$420",
-      "canvasHalf": "$350",
-      "canvasFull": "$580"
+    "original": {
+      "sold": false,
+      "medium": "Acrylic on canvas",
+      "size": "500 x 340 mm",
+      "price": "$5000"
     },
-    "size": "40 x 60cm",
-    "originalAvailable": true,
-    "originalMedium": "Acrylic on canvas",
-    "originalSize": "500 x 340 mm",
-    "originalPrice": "POA"
+    "giclee": {
+      "edition": "Limited Edition of 25",
+      "printer": "Artful Images",
+      "fullSize": "500 x 340 mm",
+      "border": 40,
+      "halfSize": "250 x 170 mm",
+      "prices": {
+        "half": "$225",
+        "full": "$420"
+      }
+    },
+    "canvas": {
+      "prices": {
+        "half": "$350",
+        "full": "$580"
+      }
+    }
   },
   {
     "id": 1,
     "src": "./1.jpeg",
-    "title": "The Boathouses",
-    "artist": "Ian Cocks",
-    "medium": "Fine art giclée print from an original acrylic painting",
-    "year": "2024",
-    "details": "Original Available",
-    "location": "Banks Peninsula",
-    "imageSize": "500 x 340 mm",
-    "totalSize": "580 x 420 mm",
-    "edition": "Open Edition",
-    "printedBy": "Artful Images",
-    "printingDetails": "Produced to the highest archival standards using pigmented inks on archival quality Epsom Velvet paper.",
-    "prices": {
-      "half": "$225",
-      "full": "$420",
-      "canvasHalf": "$350",
-      "canvasFull": "$580"
+    "title": "Duvauchelle Boatsheds",
+    "year": "2025",
+    "location": "View from Seafield Road",
+    "original": {
+      "sold": false,
+      "medium": "Acrylic on canvas",
+      "size": "500 x 281 mm",
+      "price": "$5000"
     },
-    "size": "40 x 60cm",
-    "originalAvailable": true,
-    "originalMedium": "Acrylic on canvas",
-    "originalSize": "500 x 340 mm",
-    "originalPrice": "POA"
+    "giclee": {
+      "edition": "Limited Edition of 25",
+      "printer": "Seritech Developments Limited",
+      "fullSize": "500 x 281 mm",
+      "border": 40,
+      "halfSize": "250 x 140.5 mm",
+      "prices": {
+        "half": "$225",
+        "full": "$420"
+      }
+    },
+    "canvas": {
+      "prices": {
+        "half": "$350",
+        "full": "$580"
+      }
+    }
   }
 ];
